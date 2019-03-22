4bc42b3391c473bdbc9174509646a5ffd18cab51
笹沢裕一 <werimagin@gmail.com>
2019-03-22 15:26:33 +0900

    add

--- seq2seq2.py
+++ seq2seq2.py
@@ -1,4 +1,4 @@
-log(from __future__ import unicode_literals, print_function, division
+from __future__ import unicode_literals, print_function, division
 from io import open
 import unicodedata
 import string
