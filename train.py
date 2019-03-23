7c06403aff67188b0d90c1a763359e9340911087
笹沢裕一 <werimagin@gmail.com>
2019-03-23 17:39:09 +0900

    add

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def model_handler(args,data,train=True,data_kind="train"):
                 now=time.time()
                 logger(args,"epoch,{}\tbatch\t{}\tloss:{}\ttime:{}".format(epoch,i_batch,loss.data,now-start))
                 predict,target=predict_sentence(args,predict,output_words[:,1:],t_id2word)#(batch,seq_len)
-            if i_batch%=500:
+            if i_batch%500==0:
                 for i in range(5):
                     print(predict[i])
                     print(target[i])
