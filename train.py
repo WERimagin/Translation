96e75964bda5ad4ec69d37966fcbe3233aa14412
笹沢裕一 <werimagin@gmail.com>
2019-03-25 20:13:58 +0900

    add

--- train.py
+++ train.py
@@ -78,17 +78,17 @@ def model_handler(args,data,train=True,data_kind="train"):
                 predict,target=predict_sentence(args,predict,output_words[:,1:],t_id2word)#(batch,seq_len)
             if i_batch%500==0:
                 for i in range(3):
-                    print(predict[i])
-                    print(target[i])
-                    print()
+                    logger(args,predict[i])
+                    logger(args,target[i])
+                    logger(args,"")
         else:
             predict_rate+=predict_calc(predict,output_words[:,1:])
             predict,target=predict_sentence(args,predict,output_words[:,1:],t_id2word)#(batch,seq_len)
             if i_batch==0:
                 for i in range(3):
-                    print(predict[i])
-                    print(target[i])
-                    print()
+                    logger(args,predict[i])
+                    logger(args,target[i])
+                    logger(args,"")
 
     #epochの記録
     if train:
